87c92f47ba65e9907c27ec6f85501d393c5ae0d7
Lukas Romsicki <lukas.romsicki@shopify.com>
2018-03-12 20:54:01 -0400

    Fix post ordering

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 	before_action :logged_in_user
 
 	def index
-		@posts = Post.where(user_id: current_user.friends).or(Post.where(user_id: current_user.id)).order("DATE(updated_at), id desc").paginate(page: params[:page])
+		@posts = Post.where(user_id: current_user.friends).or(Post.where(user_id: current_user.id)).order(created_at: :desc).paginate(page: params[:page])
 
 		respond_to do |format|
 			format.html
