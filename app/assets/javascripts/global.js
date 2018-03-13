91f0f102d282e6e265c6c1f34bd84fe31070f188
Lukas Romsicki <lukas.romsicki@shopify.com>
2018-03-12 20:14:30 -0400

    Improve like button functionality

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,20 +1,11 @@
 $(document).on('turbolinks:load', function() {
 	$(document).on("mouseenter", ".liked", function () {
-		$(this).find("a").html("<i class='fa fa-undo'></i>");
-		$(this).removeClass("exit");
+		$(this).html("<i class='fa fa-undo'></i>");
 	});
 
 	$(document).on("mouseleave", ".liked", function () {
-		$(this).find("a").html("<i class='fa fa-heart'></i>");
-		$(this).addClass("exit");
+		$(this).html("<i class='fa fa-heart'></i>");
 	});
-
-	$(document).on("click", ".likebutton", function (event) {
-		$(this).find("a")[0].click();
-		event.preventDefault();
-	});
-
-	$(".liked").addClass("exit");
 
 	$("#newpost").on("click", function() {
 		$("html, body").animate({ scrollTop: 0 }, 500);
