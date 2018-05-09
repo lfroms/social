10e9a5a603cf00d2740489a6be0803877e342d00
Lukas Romsicki <lukas.romsicki@shopify.com>
2018-05-09 12:24:28 -0400

    Add better profile image buttons in user settings

--- app/assets/javascripts/writepost.js
+++ app/assets/javascripts/writepost.js
@@ -1,11 +1,12 @@
 $(document).on('turbolinks:load', function () {
-	$("#new_post_add_image").click(function () {
+	$("#new_post_add_image, .file-btn").click(function () {
 		$(this).find("input[type=file]")[0].click();
 	});
 
 	$("#new_post_file").change(function () {
 
 		var file = $('#new_post_file').val();
+
 		if (file !== null) {
 			$("#new_post_drawer").animate({ height: 50 }, 300);
 
